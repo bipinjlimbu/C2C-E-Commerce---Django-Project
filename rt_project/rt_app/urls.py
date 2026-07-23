d0c750fb778d5d8f4aa8f91cd16e093ac9501e6c
bipinjlimbu <bipinlimbu712@gmail.com>
2026-07-23 17:20:41 +0545

    Products Page Added

--- rt_project/rt_app/urls.py
+++ rt_project/rt_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views.main_view import home_view
 from .views.auth_view import register_view, login_view, logout_view
 from .views.profile_view import profile_view, edit_profile_view, delete_profile_view
-from .views.product_view import add_product_view, delete_product_view, edit_product_view, is_active_toggle_view
+from .views.product_view import add_product_view, delete_product_view, edit_product_view, is_active_toggle_view, products_view
 from .views.dashboard import customer_dashboard_view
 
 urlpatterns = [
@@ -13,6 +13,7 @@ urlpatterns = [
     path('profile/', profile_view, name='profile'),
     path('profile/edit/', edit_profile_view, name='edit_profile'),
     path('profile/delete/<int:user_id>/', delete_profile_view, name='delete_profile'),
+    path('products/', products_view, name='products'),
     path('products/add/', add_product_view, name='add_product'),
     path('products/edit/<int:product_id>/', edit_product_view, name='edit_product'),
     path('products/delete/<int:product_id>/', delete_product_view, name='delete_product'),
--- rt_project/rt_app/views/product_view.py
+++ rt_project/rt_app/views/product_view.py
@@ -4,6 +4,11 @@ from django.contrib import messages
 from ..models import Product
 
 @login_required
+def products_view(request):
+    products = Product.objects.all().order_by('-created_at')
+    return render(request, 'main/products_page.html', {'products': products})
+
+@login_required
 def add_product_view(request):
     if request.user.is_staff:
         messages.error(request, "Access denied. Staff members cannot add products.")
